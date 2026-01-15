14187d294719e22fe7d474ab61621036d2151190
Fernando Henrique Marques <fernandohenriqueom@gmail.com>
2026-01-15 01:12:32 -0300

    Add image upload functionality to storage.js

---
+++ js/storage.js
@@ -0,0 +1,19 @@
+import {
+  getStorage,
+  ref,
+  uploadBytes,
+  getDownloadURL
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
+
+import { app } from "./firebase.js";
+
+const storage = getStorage(app);
+
+export async function uploadImagem({ uid, file }) {
+  const caminho = `bonecos/${uid}/${Date.now()}_${file.name}`;
+  const storageRef = ref(storage, caminho);
+
+  await uploadBytes(storageRef, file);
+
+  return await getDownloadURL(storageRef);
+}
