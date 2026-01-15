3ee0657b70d8cc182e721473661e351f12b30cf2
Fernando Henrique Marques <fernandohenriqueom@gmail.com>
2026-01-15 00:40:46 -0300

    Create bonecos.js

---
+++ js/bonecos.js
@@ -0,0 +1,29 @@
+import {
+  getFirestore,
+  collection,
+  query,
+  where,
+  getDocs,
+  orderBy
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+import { app } from "./firebase.js";
+
+const db = getFirestore(app);
+
+export async function listarBonecosDoUsuario(uid) {
+  const bonecosRef = collection(db, "bonecos");
+
+  const q = query(
+    bonecosRef,
+    where("ownerId", "==", uid),
+    orderBy("criadoEm", "desc")
+  );
+
+  const snapshot = await getDocs(q);
+
+  return snapshot.docs.map(doc => ({
+    id: doc.id,
+    ...doc.data()
+  }));
+}
