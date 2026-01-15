1a97554537d5bc972c5761b81a486704eb8c7acf
Fernando Henrique Marques <fernandohenriqueom@gmail.com>
2026-01-15 00:24:43 -0300

    Initialize Firebase app and authentication

---
+++ js/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA-awjWntNjEiMZM-RIM_93xLpEA7icYHg",
+  authDomain: "unimaginext.firebaseapp.com",
+  projectId: "unimaginext",
+  storageBucket: "unimaginext.firebasestorage.app",
+  messagingSenderId: "795705167282",
+  appId: "1:795705167282:web:3a25ac6dc1f71e6f55a8ab"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
